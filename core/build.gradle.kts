d6ea74ef6dc7bc41f5a839ff858c91491b21bd65
andylamax <andylamax@programmer.net>
2026-03-24 23:45:27 +0300

    Ready, Set, Go.

---
+++ core/build.gradle.kts
@@ -0,0 +1,77 @@
+@file:OptIn(ExperimentalComposeLibrary::class)
+
+import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
+
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("org.jetbrains.compose")
+    kotlin("plugin.compose")
+    id("tz.co.asoft.library")
+    alias(kotlinz.plugins.dokka)
+}
+
+description = "aSoft Core Rich Text Format"
+
+kotlin {
+    applyHierarchyTemplate {
+        sourceSetTrees(KotlinSourceSetTree.main, KotlinSourceSetTree.test)
+        common {
+            withAndroidTarget()
+            group("skiko") {
+                withJvm()
+                withJs()
+                withWasmJs()
+                withIosX64()
+                withIosArm64()
+                withIosSimulatorArm64()
+                withMacosX64()
+                withMacosArm64()
+            }
+        }
+    }
+
+//    androidTarget {
+        // compilations.all {
+        //    compileTaskProvider {
+        //        compilerOptions.jvmTarget = JvmTarget.JVM_17
+        //    }
+        // }
+//    }
+
+    jvm {
+        tasks.withType<Test> {
+            useJUnitPlatform()
+        }
+    }
+    js(IR) {
+        browser()
+        nodejs()
+    }
+
+    wasmJs { browser() } // until coil and kotlinx-datetime supports this, we ain't gonna
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    macosX64()
+    macosArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(kotlinx.serialization.core)
+            api(kotlinx.coroutines.core)?.because("We need to setup the supervisor job for a http polling connection")
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+            implementation(libs.kommander.core)
+            implementation(libs.kommander.coroutines)
+        }
+
+        jvmTest.dependencies {
+            implementation(kotlin("test-junit5"))
+        }
+    }
+}
