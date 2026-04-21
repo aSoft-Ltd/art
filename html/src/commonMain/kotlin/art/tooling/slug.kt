14c79d6af4c24ad5f6db5887362b5fe4d91216f2
andylamax <andylamax@programmer.net>
2026-04-22 01:53:22 +0300

    Added privacy policy for legal and identity information for guardians

--- html/src/commonMain/kotlin/art/tooling/slug.kt
+++ html/src/commonMain/kotlin/art/tooling/slug.kt
@@ -2,10 +2,10 @@ package art.tooling
 
 fun String.slug() = toList().joinToString(separator = "") {
     when (it) {
-        in listOf(' ') -> "_"
-        in listOf('/') -> "_or_"
-        in listOf('&') -> "_and_"
+        in listOf(' ') -> "-"
+        in listOf('/') -> "-or-"
+        in listOf('&') -> "-and-"
         in listOf('?', ',') -> ""
         else -> it.lowercase()
     }
-}.replace("__", "_")
+}.replace("--", "-")
